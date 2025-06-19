f0b4761449c76ff8f371da08bc6063e6932dbe24
Isabela Negri <negriisabela@icloud.com>
2025-06-19 12:03:48 -0300

    Alterações na estética

--- models.py
+++ models.py
@@ -5,4 +5,7 @@ db = SQLAlchemy()
 class Presente(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    escolhido = db.Column(db.Boolean, default=False)
+    foi_escolhido = db.Column(db.Boolean, default=False)
+    nome_comprador = db.Column(db.String(100), nullable=True)
+    telefone_comprador = db.Column(db.String(20), nullable=True)
+    quantidade = db.Column(db.Integer, nullable=True)
